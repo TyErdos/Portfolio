17da6d290df9cf6ebcf857cc059f739f37df1eba
TyErdos <erdostyler@gmail.com>
2023-07-13 12:37:00 -0400

    New css
    New styling for navbar and custom text

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,13 +7,15 @@ import './CSS/App.css'
 
 const Home = () => {
     return (  
-        <div>
+        <div className="homePage">
             <img id="mainImg" src="https://res.cloudinary.com/dl1g9gdvt/image/upload/v1683662944/Web%20Dev%20Portfolio/photo-1573655349936-de6bed86f839_4_jgvtxz.png" alt="" />
-            <section>
-                <h1 className="titleText">Tyler Erdos</h1>
-                <h3 className="titleText">Front End Web Developer</h3>  
+                <h1 className="titleText firstTitle">Tyler Erdos</h1>
+            <section className="animation">
+                <div className="secondTitle"><div>Front End Web Developer</div></div>
+                <div className="thirdTitle"><div>Full Stack Developer</div></div>
+                <div className="fourthTitle"><div>Future Team Member</div></div>
             </section>
-            <button className="mainButton btn btn-outline-light">View Projects</button>
+            <a id="mainBtnLink" href=""><button className="mainButton btn btn-outline-light">View Projects</button></a>
         </div>
     );
 }
